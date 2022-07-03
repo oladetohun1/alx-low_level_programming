53f056ff6948bce04f6f7a1ea5093269b7a7fd84
oladetohun1 <69650891+oladetohun1@users.noreply.github.com>
2022-07-03 22:28:49 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,22 +4,21 @@
 /* more headers goes there */
 
 /**
- * main - Entry level, checks if a integer is positive or negative
- * Return: Always 0
+ * main - entry point, checks if a random integer is positive or negative
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-
 	int n;
-	
+
 	srand(time(0));
-	n =  rand() - RAND_MAX / 2;
-	/*your code goes here*/
-	if (n < 0)
-		printf("%d is negative\n", n);
-	else if (n > 0)
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
 		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
 	return (0);
