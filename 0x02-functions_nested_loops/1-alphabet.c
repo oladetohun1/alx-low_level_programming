1c4073ba1b1fa29805146d85db24c90309bc6e33
oladetohun1 <oladetohunoluwakorede@gmail.com>
2022-07-05 19:18:24 +0000

    lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
- * print_alphabet -  function that prints the alphabet, in lowercase,followed by new line
- * _putchar always prints one char to std output
+ * print_alphabet -  function that prints the alphabet, 
+ * in lowercase,followed by new line
+ * _putchar - always prints one char to std output
  */
 
 void print_alphabet(void)
 {
 	int ch;
+
 	for (ch = 97; ch <= 122; ch++)
 	{
 		_putchar(ch);
