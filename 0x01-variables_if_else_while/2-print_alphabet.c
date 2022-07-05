483e2a049204a9eda55d048f1d56232658b80f5c
oladetohun1 <oladetohunoluwakorede@gmail.com>
2022-07-05 12:49:16 +0000

    Laphabets,upper and lower

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase
+ * putchar - prints single characters to the std output
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 97; ch <= 122; ch++)
+		putchar(ch);
+		putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the alphabet in lowercase, and uppercase
+ *iputchar - prints one character at a time to the screen
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+	char ch;
+
+	for (ch = 97; ch <= 122; ch++)
+		putchar(ch);
+	putchar('\n');
+	for (ch = 65; ch <= 90; ch++)
+		putchar(ch);
+		putchar('\n');
+	return (0);
+}
