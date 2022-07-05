91d725161c2f9cb3f1fd7706693e33406792a0ce
oladetohun1 <oladetohunoluwakorede@gmail.com>
2022-07-05 13:29:57 +0000

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry level,to print _putchar
+ * Putchar - prints one character at a time
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
