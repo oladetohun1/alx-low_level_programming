c46bf33d78a351b30d2bebe9f7f39324891c2e9d
oladetohun1 <oladetohunoluwakorede@gmail.com>
2022-07-07 18:42:19 -0700

    mult,reminds me of....

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - a function that multiplies two integers.
+ * @a,b:characters to be checked
+ *
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	mult = a*b;
+	return (mult);
+}
