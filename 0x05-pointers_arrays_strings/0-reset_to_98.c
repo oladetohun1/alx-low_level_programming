7b1f21345e4824c95097d1b9ed622ea4a5b60b89
oladetohun1 <oladetohunoluwakorede@gmail.com>
2022-07-12 11:27:21 +0100

    updated

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  reset_to_98 ->  a function that takes a pointer to an int as para
+ *  meter and updates the value it points to to 98.
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	int c;
+	int *p = &c;
+	*p = 98;
+}
