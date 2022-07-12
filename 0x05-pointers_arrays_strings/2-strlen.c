e52fb3dae545d5b1a0a5f7ac73146d07bfac73eb
oladetohun1 <oladetohunoluwakorede@gmail.com>
2022-07-12 21:11:32 +0000

    My first strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strlen -> A function that returns the length of a string
+ *
+ * @s: character whose len is to be cchecked 
+ *
+ * Return: Always 0
+ *
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
