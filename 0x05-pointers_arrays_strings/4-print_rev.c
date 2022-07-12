1032eba55340c7841743377162ba54fb9ae01a02
oladetohun1 <oladetohunoluwakorede@gmail.com>
2022-07-12 22:03:55 +0000

    would it wrk?

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev -> A function that prints a string in reverse,
+ * followed by a new line
+ *
+ * Return: Always 0
+ * @s: string to be reversed
+ */
+
+void print_rev(char *s)
+{
+	int b = 0;
+
+	while (s[b] ='\0')
+	{
+		b++;
+	}
+	while (b >= 0)
+	{
+		_putchar(s[b]);
+		b--;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-void reset_to_98(int *n);
+void reset_to_98(int *e);
 void swap_int(int *a, int *b);
 void _puts(char *str);
 int _putchar(char c);
 int _strlen(char *s);
-
+void print_rev(char *s);
 #endif
