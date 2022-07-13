f302a692c099a2e6f43a2dcc52693b837c87c94d
oladetohun1 <oladetohunoluwakorede@gmail.com>
2022-07-12 22:41:29 -0700

    sofast

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>8
+
+/**
+ * main - Entry level
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char alphabets;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		if (alphabets != 'q' && alphabets != 'e')
+		{
+			putchar(alphabets);
+		}
+		putchar('\n');
+		return (0);
+	}
+}
