369d6d52f47c69a08a2a648ec9d35ffcd15b6b28
oladetohun1 <oladetohunoluwakorede@gmail.com>
2022-07-14 14:29:14 -0700

    some changes made

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,16 +5,19 @@
  * _strncpy - A function that copies a string
  * @dest: where it is copied to
  * @src: where it is copied from
- * @n: number of times
+ * @n: number of charcter
+ * Return: Always 0
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0;i <=n;i++)
+	for (i = 0; i <= n + 1 && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
+	for (; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
