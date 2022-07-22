668e229f06e874181164eb45aed0ab95c87e6dd5
oladetohun1 <oladetohunoluwakorede@gmail.com>
2022-07-22 20:45:21 +0100

    hope it works

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main -> A program that prints the name followed by a new line
+* Return: an integer
+* @argc: number of cli arguments
+* @argv: a Vector that stores the cli arguments.
+*/
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+# dESCRIPTION FOR 0x0A-argc_argv
