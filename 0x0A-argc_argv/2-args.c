f4be298af244d2c6f5c843e2a47a5c0504b12824
oladetohun1 <oladetohunoluwakorede@gmail.com>
2022-07-23 01:06:00 +0100

    number 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main -> A program that prints the name followed by a new line
+* Return: an integer
+* @argc: number of cli arguments
+* @argv: a Vector that stores the cli arguments.
+*/
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i <= argc; i++)
+		printf("%d\n", argv[i]);
+	return (0);
+}
